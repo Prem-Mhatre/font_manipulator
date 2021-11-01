8a5543637b61723076c4ed96b64db19d3b592950
Prem Mhatre <83350182+Prem-Mhatre@users.noreply.github.com>
2021-11-01 18:35:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+leftWrist = 0;
+rightWrist = 0;
+difference = 0;
+noseX = 0;
+noseY = 0;
 function preload(){
 
 }
@@ -13,6 +18,13 @@ function setup(){
     pose_net.on('pose', gotPoses);
 }
 
+function draw(){
+    background("grey");
+    textSize(difference);
+    fill("black");
+    text("BTS!", noseX, noseY);
+}
+
 function modelLoaded(){
     console.log("pose net model is loaded");
 }
@@ -20,5 +32,13 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length >= 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        leftWrist = results[0].pose.leftWrist.x;
+        rightWrist = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWrist-rightWrist);
+
+        document.getElementById("font_size").innerHTML = "Font Size of the text: " + difference + "px";
     }
 }
